992bc46fff3e6fd33c2dc449726d22a32dfcec9c
sondos alansi <sondos.alansi@yahoo.com>
2018-10-18 18:52:36 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,39 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+  
+ let emp=document.querySelector('input[name="name"]').value;
+  return emp;
+}
+function addNewElementAsLi(){
+  let ul=document.querySelector('.employee-list');
+  let info=retrieveEmployeeInformation();
+  let list = document.createElement('li');
+  list.innerHTML = info;
+  ul.appendChild(list);
+  
+}
+function addNewLiOnClick(){
+   let c=document.querySelector('input[type="submit"]');
+   c.addEventListener("click",function(){
+     let ul=document.querySelector('.employee-list');
+  let info=retrieveEmployeeInformation();
+  let list = document.createElement('li');
+  list.innerHTML = info;
+  ul.appendChild(list);
+ let emp=document.querySelector('input[name="name"]');
+  emp.value="";
+   })
+  
+}
+function clearEmployeeListOnLinkClick(){
+  let ul = document.querySelector('ul')
+  let button = document.querySelector('a');
+   
+  button.addEventListener('click',function(){
+   ul.innerHTML="";
+   
+  })
+}
